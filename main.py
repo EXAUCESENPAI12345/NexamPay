d42abc6a3529c56adc379f309229e9becde5c634
EXAUCESENPAI12345 <mpikabeni56@gmail.com>
2026-07-04 18:26:40 +0100

    Ajouter du fichier main.py pour le bot

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import os
+import asyncio
+from flask import Flask, request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from flask import Flask
-import threading
 
-TOKEN = os.environ.get("8613897824:AAFce5GqBPpqQUlgBiKtx9-F39caLKX2Yak")
+TOKEN = os.environ.get("BOT_TOKEN") # Lit le token de Render
+URL = "https://nexampay.onrender.com" # Ton URL Render ici
 
-app = Flask('')
-@app.route('/')
+app = Flask(__name__)
+application = ApplicationBuilder().token(TOKEN).build()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot déployé ✅")
+
+application.add_handler(CommandHandler("start", start))
+
+@app.post(f"/webhook/{TOKEN}")
+async def webhook():
+    update = Update.de_json(request.get_json(force=True), application.bot)
+    await application.process_update(update)
+    return "ok"
+
+@app.get("/")
 def home():
     return "Bot is alive"
 
-def run_flask():
-    app.run(host='0.0.0.0', port=10000)
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot déployé sur Render ✅")
-
 if __name__ == "__main__":
-    threading.Thread(target=run_flask).start()
-    application = ApplicationBuilder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()
+    # Active le webhook au démarrage
+    asyncio.run(application.bot.set_webhook(url=f"{URL}/webhook/{TOKEN}"))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
